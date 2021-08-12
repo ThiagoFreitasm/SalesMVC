55d1e3989e6591b59a3055479e178fbc085759c8
Thiago Malaquias <Thiagofm01@hotmail.com>
2021-08-11 22:06:33 -0400

    Add project files.

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //Instanciando essa coleção
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        //Constructor sem argumentos
+        public Department() {
+        }
+        //Constructor com argumentos
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }        
+        //Method
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+    }
+}
