374b5c3c2e2f24c918244f8dd3ba1104b26fced4
Thiago Malaquias <Thiagofm01@hotmail.com>
2021-08-30 21:40:55 -0400

    test coment

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -16,6 +16,7 @@ namespace SalesWebMvc
         {
             CreateWebHostBuilder(args).Build().Run();
         }
+        //test git
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
