a3abaace3035d484e43f24e99d531a07cae8ae36
Thiago Malaquias <Thiagofm01@hotmail.com>
2021-09-08 23:02:07 -0400

    Botão EDITAR

---
+++ SalesWebMvc/Services/Exceptions/NotFoundExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoundExceptions: ApplicationException
+    {
+        //Contructor para receber a messagem
+        public NotFoundExceptions(string message) : base(message)
+        {
+
+        }
+    }
+}
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using SalesWebMvc.Services.Exceptions;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -40,6 +41,22 @@ namespace SalesWebMvc.Services
             _context.Seller.Remove(obj);
             //Confirmar a efetivacao no BD
             _context.SaveChanges();
+        }
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x=>x.Id==obj.Id))
+            {
+                throw new KeyNotFoundException("Id not found");
+            }
+            try
+            {            
+            _context.Update(obj);
+            _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
