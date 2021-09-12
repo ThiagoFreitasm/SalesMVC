61a23c2fdfb8f80ec45e133f54574e852f0be986
Thiago Malaquias <Thiagofm01@hotmail.com>
2021-09-11 20:48:48 -0400

    Excessões para Delete(referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -37,11 +37,18 @@ namespace SalesWebMvc.Services
         }
         public async Task RemoveAsync(int id)
         {
+            try
+            {            
             var obj = await _context.Seller.FindAsync(id);
             //Remoçao do obj do DBSET
             _context.Seller.Remove(obj);
             //Confirmar a efetivacao no BD
             await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales!");
+            }
         }
         public async Task UpdateAsync(Seller obj)
         {
