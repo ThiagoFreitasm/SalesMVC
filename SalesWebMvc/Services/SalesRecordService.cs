3a283d397d275153a3669f27e88cc9809ad3779f
Thiago Malaquias <Thiagofm01@hotmail.com>
2021-09-12 18:25:13 -0400

    Busca simples

---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SalesRecordService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<SalesRecord>>FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            //No return vai fazer um join com a tabela de vendedor e de departamento
+            //e ordenar decrescentemente por data. Transformando em asyncrona(async).
+            return await result
+                .Include(x=>x.Seller)
+                .Include(x=>x.Seller.Department)
+                .OrderByDescending(x=>x.Date)
+                .ToListAsync();
+        }
+    }
+}
